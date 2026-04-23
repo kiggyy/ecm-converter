97e3f727aea337a9f3003df1dc34b9a417f4cdd3
ikuzmenko <ikuzmenko@thomasmurray.com>
2026-04-23 13:02:46 +0100

    fix ZB skip 999 rows

--- generator_zb.py
+++ generator_zb.py
@@ -42,8 +42,7 @@ class GeneratorZB:
 
             for item in items:
                 if item.Fdr >= 999:
-                    feeder = 48
-                    ignore = '*'
+                    continue
                 else:
                     feeder = item.Fdr
                     ignore = ''
